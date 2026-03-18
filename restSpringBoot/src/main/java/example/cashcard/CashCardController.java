fdaa0727a09c1097f00f689700aa8449c821bfe2
Sumin Oh <kurkmin@gmail.com>
2026-03-18 15:59:41 +0000

    post added

--- restSpringBoot/src/main/java/example/cashcard/CashCardController.java
+++ restSpringBoot/src/main/java/example/cashcard/CashCardController.java
@@ -2,9 +2,14 @@ package example.cashcard;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+import java.net.URI;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 import java.util.Optional;
 
@@ -31,5 +36,15 @@ class CashCardController {
         // } else {
         //     return ResponseEntity.notFound().build();
         // }
+    }
+
+    @PostMapping
+    private ResponseEntity<Void> createCashCard(@RequestBody CashCard newCashCardRequest, UriComponentsBuilder ucb) {
+        CashCard savedCashCard = cashCardRepository.save(newCashCardRequest);
+        URI locationOfNewCashCard = ucb
+                 .path("cashcards/{id}")
+                 .buildAndExpand(savedCashCard.id())
+                 .toUri();
+        return ResponseEntity.created(locationOfNewCashCard).build();
     }
 }
